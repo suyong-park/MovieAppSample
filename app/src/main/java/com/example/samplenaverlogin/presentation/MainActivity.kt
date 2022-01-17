abd77ebfae3ed804e97a5ca79dcfb55c0756a5f5
박수용 <spdlqjfire@naver.com>
2022-01-17 20:57:33 +0900

    init project

---
+++ app/src/main/java/com/example/samplenaverlogin/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.samplenaverlogin.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.samplenaverlogin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
